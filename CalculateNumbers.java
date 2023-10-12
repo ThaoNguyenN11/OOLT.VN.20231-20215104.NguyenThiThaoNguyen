784b8587acace01e7f4dd81cdceed24c2620250e
ThaoNguyenN11 <nguyenthao123lgf1@gmail.com>
2023-10-12 16:56:12 +0700

    Complete Assignment 2.2.5

--- CalculateNumbers.java
+++ CalculateNumbers.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
-public class CalculateNumbers {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the first number: ");
-		double num1 = input.nextDouble();
-		System.out.print("Enter the second number: ");
-		double num2 = input.nextDouble();
-		double sum = num1 + num2;
-		double product = num1 * num2;
-		double difference = num1 - num2;
-		double quotient = num1 / num2;
-		System.out.println("Sum: " + sum);
-		System.out.println("Product: " + product);
-		System.out.println("Difference: " + difference);
-		System.out.println("Quotient: " + quotient);
-	}
 
+public class CalculateNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first double number: ");
+        String strNum1 = scanner.nextLine();
+        double num1 = Double.parseDouble(strNum1);
+       
+	    System.out.println("Enter the second double number: ");
+        String strNum2 = scanner.nextLine();
+        double num2 = Double.parseDouble(strNum2);
+
+        // Calculate the sum, difference, product, and quotient of the two double numbers
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("The sum of the two numbers is: " + sum);
+        System.out.println("The difference of the two numbers is: " + difference);
+        System.out.println("The product of the two numbers is: " + product);
+        System.out.println("The quotient of the two numbers is: " + quotient);
+        scanner.close();
+    }
 }
---
+++ iregnf/src/iregnf/herer.java
@@ -0,0 +1,5 @@
+package iregnf;
+
+public class herer {
+
+}
