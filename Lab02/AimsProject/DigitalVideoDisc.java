b9931bbfdfe38f7a52ab319feeeb6901f76df777
ThaoNguyenN11 <nguyenthao123lgf1@gmail.com>
2023-10-28 21:19:09 +0700

    Section 16

---
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -0,0 +1,68 @@
+public class DigitalVideoDisc {
+
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    public int id = nbDigitalVideoDisc;
+
+    
+    public DigitalVideoDisc( String category,String title, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+		nbDigitalVideoDisc++;
+	}
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+		nbDigitalVideoDisc++;
+	}
+
+    public String getTitle() {
+        return title;
+    }
+    
+	public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
