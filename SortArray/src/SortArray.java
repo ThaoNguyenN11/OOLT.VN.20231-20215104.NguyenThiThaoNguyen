d4ae7ddbcd721176c355c263230dbff44267f51d
ThaoNguyenN11 <nguyenthao123lgf1@gmail.com>
2023-12-15 11:11:17 +0700

    lab01

---
+++ SortArray/src/SortArray.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the element at index " + i + ": ");
+            array[i] = scanner.nextInt();
+        }
+        Arrays.sort(array);
+
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += array[i];
+        }
+        double average = (double) sum / size;
+
+        System.out.println("Sorted array: " + Arrays.toString(array));
+        System.out.println("Sum of array elements: " + sum);
+        System.out.println("Average value of array elements: " + average);
+
+    }
+}
+
