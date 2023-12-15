70e6975bd08f9325968d2e1833d2d565dca0ebdb
ThaoNguyenN11 <nguyenthao123lgf1@gmail.com>
2023-12-15 11:05:31 +0700

    lab01

---
+++ DrawTriangle/src/DrawTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DrawTriangle{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter height of n stars: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
