cb157029d1dcde5d0562190c56fe9d507e0a6a4d
ThaoNguyenN11 <nguyenthao123lgf1@gmail.com>
2023-12-20 19:32:58 +0700

    lab02

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -3,6 +3,47 @@ public class DigitalVideoDisc {
     private String category;
     private String director;
     private int length;
+
+    public java.lang.String getTitle() {
+        return title;
+    }
+
+    public void setTitle(java.lang.String title) {
+        this.title = title;
+    }
+
+    public java.lang.String getCategory() {
+        return category;
+    }
+
+    public void setCategory(java.lang.String category) {
+        this.category = category;
+    }
+
+    public java.lang.String getDirector() {
+        return director;
+    }
+
+    public void setDirector(java.lang.String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
     private float cost;
 
 }
